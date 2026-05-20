959767a5b21a2f84cc2349b8db2765ca961a68b5
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-05-20 14:30:42 +0900

    テキストの詳細ページ作成

---
+++ app/controllers/textbooks_controller.rb
@@ -0,0 +1,21 @@
+class TextbooksController < ApplicationController
+  def index
+    
+  end
+
+  def show
+  
+  end
+
+  def reading
+    
+  end
+
+  def listening
+    
+  end
+
+  def shadowing
+    
+  end
+end
