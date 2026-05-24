88ac202c203ce454500042a416471d2167fb0a11
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-05-24 18:33:23 +0900

    気に入りボタン

--- app/javascript/likeBtn.js
+++ app/javascript/likeBtn.js
@@ -1,13 +1,27 @@
 document.addEventListener("turbo:load", () => {
   const likeBtn = document.querySelector(".my-list");
+  const modalWindow = document.getElementById('modal-window');
+  const modalContent = document.querySelector('.modal-content');
+  const modalOkayBtn = document.getElementById('modal-okay-button');
+  const modalMyList = document.querySelector('.modal-my-list h4');
+
   if (!likeBtn) return;
 
   likeBtn.addEventListener("click", () => {
+    modalWindow.classList.add('active');
     likeBtn.classList.toggle("active");
+    
     if (likeBtn.classList.contains("active")) {
       likeBtn.textContent = "マイリスト登録済";
+      modalMyList.textContent = 'マイリストに登録しました。'
+      likeBtn.innerHTML = `マイリストから削除する`
     } else {
-      likeBtn.innerHTML = `マイリストに追加 <i class="bi bi-heart"></i>`;
+      modalMyList.textContent = 'マイリストから削除しました。'
+      likeBtn.innerHTML = `マイリストに登録する`
     }
   });
+
+  modalOkayBtn.addEventListener("click", () => {
+    modalWindow.classList.remove('active');
+  })
 });
