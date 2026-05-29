331fe4821e8feed684a5c32da4a65b1195328e69
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-05-29 13:37:21 +0900

    19_UIの修正

--- app/javascript/sidebar.js
+++ app/javascript/sidebar.js
@@ -1,16 +1,26 @@
 
 document.addEventListener('turbo:load', () => {
   const sideBar = document.querySelector('.side-bar')
+  const sideBarCloseBtn = document.querySelector('.side-bar h1')
+  const BackgroundContainer = document.querySelector('.ja-en-container');
   const closeBtn = document.querySelector('.fa-angle-up');
   const openBtn = document.querySelector('.side-bar-toggle');
+  const AudioBtn = document.querySelector('.side-bar-toggle h1');
 
-  if (!openBtn || !sideBar) return;
+  if (!openBtn || !sideBar || !sideBarCloseBtn) return;
 
   openBtn.addEventListener('click', () => {
     sideBar.classList.add('open');
+    AudioBtn.classList.add('hide');
   })
 
-  closeBtn.addEventListener('click', () => {
+  sideBarCloseBtn.addEventListener('click', () => {
     sideBar.classList.remove('open');
+    AudioBtn.classList.remove('hide');
+  })
+
+  BackgroundContainer.addEventListener('click', () => {
+    sideBar.classList.remove('open');
+    AudioBtn.classList.remove('hide');
   })
 })
