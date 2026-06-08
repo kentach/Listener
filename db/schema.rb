fc3791d40ffac4493cbe6ff5fa11ace8e83b2d41
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-08 16:37:24 +0900

    change_email_column_to_student_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_05_025558) do
+ActiveRecord::Schema[7.2].define(version: 2026_06_08_064851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "allowed_students", force: :cascade do |t|
+    t.string "student_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["student_id"], name: "index_allowed_students_on_student_id", unique: true
+  end
 
   create_table "audios", force: :cascade do |t|
     t.bigint "lesson_id", null: false
@@ -76,7 +83,6 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_05_025558) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "eiken_level", default: "", null: false
     t.string "reset_password_token"
@@ -84,8 +90,9 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_05_025558) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "student_id", default: "", null: false
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["student_id"], name: "index_users_on_student_id", unique: true
   end
 
   add_foreign_key "audios", "lessons"
