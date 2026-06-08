572ef358e1fd29d13d5246079316924adf5a21fd
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-08 18:28:23 +0900

    fix_rubocop

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,7 +2,6 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: "管理画面" do
-
     columns do
       column do
         panel "生徒数" do
@@ -23,6 +22,5 @@ ActiveAdmin.register_page "Dashboard" do
         end
       end
     end
-
   end
 end
