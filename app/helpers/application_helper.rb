3cab5aa3dbeddd8459537a22798ea81c67b72e30
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-13 19:53:06 +0900

    fix:header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
     base_title = "LISTENER 2.0"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
+
+  def hide_header?
+    (controller_name == "sessions" && action_name == "new") ||
+    (controller_name == "registrations" && action_name == "new") ||
+    (controller_name == "audios" && action_name == "show")
+  end
 end
