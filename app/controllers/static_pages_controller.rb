05a36ba7801b37ad41220597d6b6825cf56b7a18
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-17 14:30:18 +0900

    replace_all_audios

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   def top
     @fiveStage_textbooks = Textbook.where(series: "5ステージ")
+    @eigoMimi_textbooks = Textbook.where(series: "英語耳")
     @textbooks = Textbook.all
   end
 end
